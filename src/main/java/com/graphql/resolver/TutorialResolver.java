6dc3e3ace75731857344a079ba11ebc17d5f8159
Tomas Pesek <pesekt@gmail.com>
2022-04-08 09:12:08 +0200

    resolver is not needed

--- src/main/java/com/graphql/resolver/TutorialResolver.java
+++ src/main/java/com/graphql/resolver/TutorialResolver.java
@@ -7,6 +7,7 @@ import com.graphql.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+//FIXME: this resolver is not needed, when Tutorial is queried it uses getAuthor() from Tutorial class.
 @Component
 public class TutorialResolver implements GraphQLResolver<Tutorial> {
 	@Autowired
@@ -16,7 +17,7 @@ public class TutorialResolver implements GraphQLResolver<Tutorial> {
 		this.authorRepository = authorRepository;
 	}
 
-	public Author getAuthor(Tutorial tutorial) {
-		return authorRepository.findById(tutorial.getAuthor().getId()).orElseThrow(null);
-	}
+//	public Author getAuthor(Tutorial tutorial) {
+//		return authorRepository.findById(tutorial.getAuthor().getId()).orElseThrow(null);
+//	}
 }
